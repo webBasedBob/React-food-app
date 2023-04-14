160fea911ced7802a3e54820ee6c8a8173d3d67c
webBasedBob <catalin.b.pop97@gmail.com>
2023-04-14 16:08:46 +0300

    added a very simple express server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+app.listen(3000);
